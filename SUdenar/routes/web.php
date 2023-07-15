0604aeff2ba38ebab90ce7f0f16b454637a3def5
RonnyAndres <76404004+RonnyAndres@users.noreply.github.com>
2023-07-15 12:51:52 +0000

    Adicione ruta

--- SUdenar/routes/web.php
+++ SUdenar/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\ProyectosController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+// Ruta de Proyectos
+Route::resource('proyectos', ProyectosController::class)->names('admin.proyectos');
+
